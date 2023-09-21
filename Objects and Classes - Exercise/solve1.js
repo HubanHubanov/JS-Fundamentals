b96de045c52b71250a21e59fc23203a65c287b58
Huban Hubanov <huban.hubanov@gmail.com>
2023-09-21 14:42:15 +0100

    test1

---
+++ Objects and Classes - Exercise/solve1.js
@@ -0,0 +1,22 @@
+function employees(arr) {
+  let restArr = [];
+
+  for (let line of arr) {
+    let obj = {
+      name: line,
+      personalNumber: line.length,
+    };
+
+    restArr.push(obj);
+  }
+  restArr.forEach((x) =>
+    console.log(`Name: ${x.name} -- Personal Number: ${x.personalNumber}`)
+  );
+}
+
+employees([
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
