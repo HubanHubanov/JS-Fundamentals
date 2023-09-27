01774153e561e9975d2aaf50e5976d068de8f34a
Huban Hubanov <huban.hubanov@gmail.com>
2023-09-27 12:45:26 +0100

    add 03.AdressBook2

---
+++ 13.Associative Arrays - Lab/03.AdressBook2.js
@@ -0,0 +1,25 @@
+function adressBook(input) {
+  let obj = {};
+
+  for (let el of input) {
+    let tokens = el.split(":");
+    let name = tokens[0];
+    let adress = tokens[1];
+
+    obj[name] = adress;
+  }
+
+  let entries = Object.entries(obj);
+  let entriesSorted = entries.sort((a, b) => a[0].localeCompare(b[0]));
+
+  for (let [key, value] of entries) {
+    console.log(`${key} -> ${value}`);
+  }
+}
+
+adressBook([
+  "Tim:Doe Crossing",
+  "Bill:Nelson Place",
+  "Peter:Carlyle Ave",
+  "Bill:Ornery Rd",
+]);
