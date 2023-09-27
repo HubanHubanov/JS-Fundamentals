99908878ad9f6d946afdd7a019bb0619c1b0c810
Huban Hubanov <huban.hubanov@gmail.com>
2023-09-27 14:49:09 +0100

    add 04.StorageMap

---
+++ 13.Associative Arrays - Lab/04.StorageMap.js
@@ -0,0 +1,20 @@
+function storage(data) {
+  let result = new Map();
+
+  for (el of data) {
+    let [name, quantity] = el.split(" ");
+    quantity = Number(quantity);
+
+    if (!result.has(name)) {
+      result.set(name, quantity);
+    } else {
+      result.set(name, result.get(name) + quantity);
+    }
+  }
+
+  for (let [key, value] of result) {
+    console.log(`${key} -> ${value}`);
+  }
+}
+
+storage(["tomatoes 10", "coffee 5", "olives 100", "coffee 40"]);
