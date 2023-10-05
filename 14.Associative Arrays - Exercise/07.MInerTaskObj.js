952c78cf1f428f7ac5ba1eed33ea8193271b3767
Huban Hubanov <huban.hubanov@gmail.com>
2023-10-05 12:05:36 +0100

    add 07.MinerTaskObj

---
+++ 14.Associative Arrays - Exercise/07.MInerTaskObj.js
@@ -0,0 +1,22 @@
+function minerTask(input) {
+  let obj = {};
+
+  for (let i = 0; i < input.length; i += 2) {
+    let resource = input[i];
+    let quantity = Number(input[i + 1]);
+
+    if (!obj.hasOwnProperty(resource)) {
+      obj[resource] = quantity;
+    } else {
+      obj[resource] = obj[resource] + quantity;
+    }
+  }
+
+  for (let key in obj) {
+    console.log(`${key} -> ${obj[key]}`);
+  }
+}
+
+// minerTask(["Gold", "155", "Silver", "10", "Copper", "17"]);
+
+minerTask(["gold", "155", "silver", "10", "copper", "17", "gold", "15"]);
