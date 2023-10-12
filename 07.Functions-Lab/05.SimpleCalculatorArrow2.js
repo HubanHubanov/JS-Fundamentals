26e9b0c4a87ef5a493403a874f06f87542ed171b
Huban Hubanov <huban.hubanov@gmail.com>
2023-10-12 01:01:44 +0100

    add 05.SimpleCalculatorArrow

---
+++ 07.Functions-Lab/05.SimpleCalculatorArrow2.js
@@ -0,0 +1,19 @@
+function simpleCalculator(firstNum, secondNum, operator) {
+  let multiply = (firstNum, secondNum) => firstNum * secondNum;
+  let divide = (firstNum, secondNum) => firstNum / secondNum;
+  let add = (firstNum, secondNum) => firstNum + secondNum;
+  let subtract = (firstNum, secondNum) => firstNum - secondNum;
+
+  switch (operator) {
+    case "multiply":
+      return multiply(firstNum, secondNum);
+    case "divide":
+      return divide(firstNum, secondNum);
+    case "add":
+      return add(firstNum, secondNum);
+    case "subtract":
+      return subtract(firstNum, secondNum);
+  }
+}
+
+console.log(simpleCalculator(5, 5, "multiply"));
