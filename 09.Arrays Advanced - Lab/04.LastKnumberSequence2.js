8db5aadfd7e4585cc18c0b526481e90ecff755b0
Huban Hubanov <huban.hubanov@gmail.com>
2023-10-17 12:23:40 +0100

    add 04.LastKnumberSequence2

---
+++ 09.Arrays Advanced - Lab/04.LastKnumberSequence2.js
@@ -0,0 +1,28 @@
+function lastKnumberSequence(length, k) {
+  let res = [];
+
+  for (let i = 0; i < length; i++) {
+    if (i === 0) {
+      res.push(1);
+      continue;
+    }
+    let sumArr = [];
+    if (i >= k) {
+      sumArr = res.slice(i - k, i);
+    } else {
+      sumArr = res.slice(0, i);
+    }
+
+    let sum = 0;
+    for (let el of sumArr) {
+      sum += el;
+    }
+    res.push(sum);
+  }
+
+  console.log(res);
+}
+
+lastKnumberSequence(6, 3);
+
+lastKnumberSequence(8, 2);
