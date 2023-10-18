43dd8b9405ef2d4aa1a0109e9e6045055fa27645
Huban Hubanov <huban.hubanov@gmail.com>
2023-10-18 12:18:05 +0100

    add  02.DistinctArray3

---
+++ 10.Arrays Advanced - Exercise/02.DIstinctArray3.js
@@ -0,0 +1,12 @@
+function distinctArray(arr) {
+  let res = [];
+
+  for (let el of arr) {
+    if (!res.includes(el)) {
+      res.push(el);
+    }
+  }
+
+  console.log(res.join(" "));
+}
+distinctArray([7, 8, 9, 7, 2, 3, 4, 1, 2]);
