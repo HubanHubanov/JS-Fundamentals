a4efd36305aac109a8f2c4a35a57daac227f8491
Huban Hubanov <huban.hubanov@gmail.com>
2023-11-13 12:04:05 +0000

    update

--- 16.Text Processing - Exercise/08.HardWordRegex.js
+++ 16.Text Processing - Exercise/08.HardWordRegex.js
@@ -1,18 +1,25 @@
-function hardWords(input) {
+function hardWord(input) {
   let str = input[0];
+  let strArr = str.split(" ");
   let wordsArr = input[1];
 
-  
-  for (let word of wordsArr) {
-    let wordToReplace = "_".repeat(word.length);
-    
-    str = str.replace(wordToReplace, word);
+  let pattern = /[_]+/g;
+
+  let match = pattern.exec(str);
+
+  while (match) {
+    for (let el of wordsArr) {
+      if (el.length === match[0].length) {
+        str = str.replace(match, el);
+      }
+    }
+    match = pattern.exec(str);
   }
-  console.log(` str);
- 
+
+  console.log(str);
 }
 
-hardWords([
+hardWord([
   "Hi, grandma! I'm so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother's ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.",
   ["pie", "bring", "glad", "During", "amazing", "pharmacist", "sprained"],
 ]);
