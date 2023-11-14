d0e4cc398bc0b62d9693690f35f23f27186637c7
Huban Hubanov <huban.hubanov@gmail.com>
2023-11-14 10:58:16 +0000

    add 09.PasswordGenerator

---
+++ 16.Text Processing - Exercise/09.PasswordGenerator.js
@@ -0,0 +1,36 @@
+function passwordGenerator(str) {
+  let firsStr = str[0];
+  let secondStr = str[1];
+  let word = str[2];
+  let wordUpperCase = word.toUpperCase();
+  let combinedStr = firsStr.concat(secondStr);
+  let index = 0;
+
+  for (let ch of combinedStr) {
+    if (
+      ch.charCodeAt() === 97 ||
+      ch.charCodeAt() === 101 ||
+      ch.charCodeAt() === 105 ||
+      ch.charCodeAt() === 111 ||
+      ch.charCodeAt() === 117
+    ) {
+      combinedStr = combinedStr.replace(ch, wordUpperCase[index]);
+      index++;
+      if (index === wordUpperCase.length) {
+        index = 0;
+      }
+    }
+  }
+  let res = combinedStr.split("").reverse().join("");
+  console.log(`Your generated password is ${res}`);
+}
+
+passwordGenerator(["ilovepizza", "ihatevegetables", "orange"]);
+
+passwordGenerator(["easymoneyeazylife", "atleasttencharacters", "absolute"]);
+
+passwordGenerator([
+  "areyousureaboutthisone",
+  "notquitebutitrustyou",
+  "disturbed",
+]);
