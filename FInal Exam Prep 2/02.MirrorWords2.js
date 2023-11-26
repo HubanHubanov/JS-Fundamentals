8c747046508e2e454409595746ce2729eea50c7f
Huban Hubanov <huban.hubanov@gmail.com>
2023-11-26 18:16:30 +0000

    add 02.MirrorWords

---
+++ FInal Exam Prep 2/02.MirrorWords2.js
@@ -0,0 +1,49 @@
+function mirrorWords(input) {
+    let str = input[0];
+  
+    let pattern =
+      /([@#])(?<firstWord>[A-Za-z]{3,})\1\1(?<secondWord>[A-Za-z]{3,})\1/g;
+  
+    let match = pattern.exec(str);
+  
+    let pairsCount = 0;
+    let mirrorWords = 0;
+    let buff = [];
+    while (match) {
+      pairsCount++;
+  
+      let firstWord = match.groups.firstWord;
+      let secondWord = match.groups.secondWord;
+  
+      let secondWordReverse = secondWord.split("").reverse().join("");
+  
+      if (firstWord === secondWordReverse) {
+        mirrorWords++;
+        buff.push(`${firstWord} <=> ${secondWord}`);
+      }
+  
+      match = pattern.exec(str);
+    }
+  
+    if (pairsCount === 0) {
+      console.log("No word pairs found!");
+      console.log("No mirror words!");
+    } else {
+      console.log(`${pairsCount} word pairs found!`);
+      if (mirrorWords === 0) {
+        console.log("No mirror words!");
+      } else {
+        console.log("The mirror words are:");
+        console.log(buff.join(", "));
+      }
+    }
+  }
+  
+  mirrorWords([
+    "@mix#tix3dj#poOl##loOp#wl@@bong&song%4very$long@thong#Part##traP##@@leveL@@Level@##car#rac##tu@pack@@ckap@#rr#sAw##wAs#r#@w1r",
+  ]);
+  
+  mirrorWords(["#po0l##l0op# @bAc##cAB@ @LM@ML@ #xxxXxx##xxxXxx# @aba@@ababa@"]);
+  
+  mirrorWords(["#lol#lol# @#God@@doG@# #abC@@Cba# @Xyu@#uyX#"]);
+  
